0d6f59378f6f490f665faf8c302790589f8b07f9
RupamAnthony <royrupamanthony@gmail.com>
2024-04-26 22:35:31 +0600

    sixth commit

--- api.js
+++ api.js
@@ -26,3 +26,22 @@ function display(items) {
             newDiv.classList.add("innerStyle");
             container.appendChild(newDiv);
         } else {
+            if (r === maxToShow) {
+                var showMoreButton = document.createElement("button");
+                showMoreButton.textContent = "Show More";
+                showMoreButton.onclick = function() {
+                    for (var m = maxToShow; m < items.length; m++) {
+                        var newDiv = document.createElement("div");
+                        newDiv.innerHTML = `Meal ID: <b>${items[m].idMeal}</b> <br>  Meal Title: <b>${items[m].strMeal}</b><br>
+                                            <img src="${items[m].strMealThumb}"><br>
+                                            <b>Cooking Instruction:</b> ${items[m].strInstructions}<br><br>`;
+                        newDiv.classList.add("innerStyle");
+                        container.appendChild(newDiv);
+                    }
+                    showMoreButton.style.display = "none";
+                };
+                container.appendChild(showMoreButton);
+            }
+        }
+    }
+}
