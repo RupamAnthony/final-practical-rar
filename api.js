592b38a5170be93badc8348f35d0d58e5cd5dd47
RupamAnthony <royrupamanthony@gmail.com>
2024-04-26 21:07:47 +0600

    fourth commit

---
+++ api.js
@@ -0,0 +1,15 @@
+function connect() {
+    var searchTerm = document.getElementById("searchBox").value;
+    document.getElementById("searchBox").value = "";
+    var url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`;
+
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => display(data.meals));
+}
+
+
+function display(items) {
+    var container = document.getElementById("container");
+    container.textContent = "";
